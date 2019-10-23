49540ff047d1e0568c856bb3ab5059af931ed65e
Steve <svsoft@free.fr>
2019-10-23 18:25:00 +0200

    (feat): add sample program

---
+++ samples/project_cpp/pgm_1.cpp
@@ -0,0 +1,35 @@
+//#NAME PGM1
+//#OPTMOD PACKSTRUCT(1)
+
+#include <stdio.h>
+
+#include <errno.h>
+
+#include <iostream>                    
+#include <iomanip>                     
+#include <fstream>      
+
+#include <qlicobjd.h>
+#include <mih/matsobj.h>
+#include <mih/rslvsp.h>
+#include <mih/setsppfp.h>
+#include <mih/setsppo.h>
+#include <qusmiapi.h>
+
+int main(int argc , char * argv[]) 
+{    
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
