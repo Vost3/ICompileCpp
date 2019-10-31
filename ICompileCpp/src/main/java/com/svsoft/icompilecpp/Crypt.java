00be3eeadc841a4ffb63a08b919d5fd0e68c2d6b
Steve <svsoft@free.fr>
2019-10-31 16:49:32 +0100

    (fix): return null if exception when encrypt of decrypt

--- ICompileCpp/src/main/java/com/svsoft/icompilecpp/Crypt.java
+++ ICompileCpp/src/main/java/com/svsoft/icompilecpp/Crypt.java
@@ -12,12 +12,18 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -160,18 +166,24 @@ public class Crypt {
      * @return
      * @throws Exception 
      */
-    public String encrypt(String str) throws Exception {
-        Cipher ecipher = null;
-        ecipher = Cipher.getInstance("AES");
-        ecipher.init(Cipher.ENCRYPT_MODE, Skey);
-        // Encode the string into bytes using utf-8
-        byte[] utf8 = str.getBytes("UTF8");
-
-        // Encrypt
-        byte[] enc = ecipher.doFinal(utf8);
-
-        // Encode bytes to base64 to get a string
-        return new sun.misc.BASE64Encoder().encode(enc);
+    public String encrypt(String str) {
+        try {
+            Cipher ecipher = null;
+            ecipher = Cipher.getInstance("AES");
+            ecipher.init(Cipher.ENCRYPT_MODE, Skey);
+            // Encode the string into bytes using utf-8
+            byte[] utf8 = str.getBytes("UTF8");
+            
+            // Encrypt
+            byte[] enc = ecipher.doFinal(utf8);
+            
+            // Encode bytes to base64 to get a string
+            return new sun.misc.BASE64Encoder().encode(enc);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | UnsupportedEncodingException | IllegalBlockSizeException | BadPaddingException ex) {
+            //@TODO : log that
+            //Logger.getLogger(Crypt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     /** 
@@ -180,17 +192,26 @@ public class Crypt {
      * @return
      * @throws Exception 
      */
-    public String decrypt(String str) throws Exception {
-        Cipher dcipher = null;
-        dcipher = Cipher.getInstance("AES");              
-        dcipher.init(Cipher.DECRYPT_MODE, Skey);
-        
-        // Decode base64 to get bytes
-        byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
-                
-        byte[] utf8 = dcipher.doFinal(dec);
-
-        // Decode using utf-8
-        return new String(utf8, "UTF8");
+    public String decrypt(String str) {
+        try {
+            Cipher dcipher = null;
+            dcipher = Cipher.getInstance("AES");
+            dcipher.init(Cipher.DECRYPT_MODE, Skey);
+            
+            // Decode base64 to get bytes
+            byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+            
+            byte[] utf8 = dcipher.doFinal(dec);
+            
+            // Decode using utf-8
+            return new String(utf8, "UTF8");
+        } catch (InvalidKeyException | UnsupportedEncodingException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            //@TODO : log that
+            //Logger.getLogger(Crypt.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalBlockSizeException | BadPaddingException | IOException ex) {
+            //@TODO : log that
+            //Logger.getLogger(Crypt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
